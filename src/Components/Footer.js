ceb401e249d6b84390e96d35d02d4457246d3908
thalisonsantos91 <thalisoncampelo@hotmail.com>
2025-04-02 22:51:53 -0300

    Incluindo o Navbar e o footer.

---
+++ src/Components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer(){
+    return (
+      <div className="text-center p-4 border-top bg-light mt-3">
+        Catálogo Online - Todos os direitos reservados
+        </div>
+      )
+  }
